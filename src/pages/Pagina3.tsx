3e5c27e5da6b5d1b62a93c682113ee8304dc3446
kirapablinski <163457398+kirapablinski@users.noreply.github.com>
2024-07-23 20:35:46 -0400

    Update Pagina3.tsx

--- src/pages/Pagina3.tsx
+++ src/pages/Pagina3.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Form from 'react-bootstrap/form'
+import Form from 'react-bootstrap/Form'
 import  Button  from 'react-bootstrap/Button'
 import { Persona } from '@/interface/iPersona'
 import { registrarPersona } from '@/Firebase/Promesas'
